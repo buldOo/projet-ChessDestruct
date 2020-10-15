f7da544bacff8903573d4019b7ae44018b24cf5d
killian gay <killian.gay28@gmail.com>
2020-10-15 17:32:02 +0200

    ajoux nouvelles variables globales

---
+++ src/com/company/variables_globals.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class variables_globals {
+    public static int RY = 6;
+    public static int RX = 6;
+    public static int BY = 5;
+    public static int BX = 6;
+}

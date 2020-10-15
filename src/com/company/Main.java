07a35eb33d52a016a5e8767b29f5916ebb55bc8d
killian gay <killian.gay28@gmail.com>
2020-10-15 17:32:33 +0200

    optimisation du code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,53 +1,116 @@
 package com.company;
 
-import java.awt.*;
-import java.io.Console;
 import java.util.Scanner;
 
 public class Main {
     //fonction
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         int boucle = 0;
+        int boucle2 = 0;
+        int A = 0;
         Scanner myScan = new Scanner(System.in);
 
         //print menu
         fonction.writeMenu();
-        while (boucle < 1){
-            while (!myScan.hasNextInt()) {
-                System.out.println("çe n'est pas un nombre");
-                myScan.next();
-            }
-            //used to recover user input
-            int inputUtilisateur = myScan.nextInt();
+        while (boucle < 1) { // create a loop for the menu
+            String inputUser = myScan.nextLine();
 
-            if (inputUtilisateur == 1){
+            if (inputUser.equals("rules")) {
+                // access the game's rules
                 fonction.clearConsole();
                 fonction.rules();
-                System.out.println("tapez 1 pour revenir au menu");
-                myScan.nextInt();
-                if (inputUtilisateur == 1){
-                    fonction.clearConsole();
-                    fonction.writeMenu();
-                }
+                fonction.optionMenu();
             }
-            if (inputUtilisateur == 2){
+            else if (inputUser.equals("play")) {
+                // start the game
                 fonction.clearConsole();
-                fonction.tableau();
+                fonction.initBoard();
                 fonction.playerChoice(1, 2);
-                System.out.println("tapez 2 pour revenir au menu");
-                myScan.nextInt();
-                if (inputUtilisateur == 2){
-                    fonction.clearConsole();
-                    fonction.writeMenu();
+                fonction.keys();
+                fonction.optionMenu();
+
+                while (boucle2 < 1){
+                    inputUser = myScan.nextLine();
+
+                    if (inputUser.equals("d")) {
+                        variables_globals.RX++;
+                        fonction.clearConsole();
+                        fonction.showPlayer();
+                        fonction.keys();
+
+                    }
+                    if (inputUser.equals("q")) {
+                        variables_globals.RX--;
+                        fonction.clearConsole();
+                        fonction.showPlayer();
+                        fonction.keys();
+
+
+                    }
+                    if (inputUser.equals("s")) {
+                        variables_globals.RY++;
+                        fonction.clearConsole();
+                        fonction.showPlayer();
+                        fonction.keys();
+
+
+                    }
+                    if (inputUser.equals("z")) {
+                        variables_globals.RY--;
+                        fonction.clearConsole();
+                        fonction.showPlayer();
+                        fonction.keys();
+
+
+                    }
+                    if (inputUser.equals("k")) {
+                        variables_globals.BY++;
+                        fonction.showPlayer();
+                        fonction.keys();
+
+
+                    }
+                    if (inputUser.equals("i")) {
+                        variables_globals.BY--;
+                        fonction.showPlayer();
+                        fonction.keys();
+
+
+                    }
+                    if (inputUser.equals("l")) {
+                        variables_globals.BX++;
+                        fonction.showPlayer();
+                        fonction.keys();
+
+
+                    }
+                    if (inputUser.equals("j")) {
+                        variables_globals.BX--;
+                        fonction.showPlayer();
+                        fonction.keys();
+
+                    }
+
+
                 }
+
             }
-            if (inputUtilisateur == 3){
+            else if (inputUser.equals("leave")) {
+                // close the game
+                fonction.clearConsole();
                 System.out.println("le logiciel vas se fermer");
                 boucle = 1;
             }
+            else if (inputUser.equals("menu")) {
+                // write menu
+                fonction.clearConsole();
+                fonction.writeMenu();
+            }
         }
+
     }
 }
+
 
